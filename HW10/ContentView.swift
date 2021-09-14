c5071daf0b7e4dd0e5777621048653def0eb9840
Trishkin Konstantin <kostyatrishkin@gmail.com>
2021-09-14 21:02:51 +0300

    Initial Commit

---
+++ HW10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW10
+//
+//  Created by Константин Тришкин on 9/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW10/HW10App.swift
@@ -0,0 +1,17 @@
+//
+//  HW10App.swift
+//  HW10
+//
+//  Created by Константин Тришкин on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct HW10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
